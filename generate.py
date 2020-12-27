c0505ea2b32218a522267d8f50e6cfb4746dfc7c
Enkhai <aerogtx@gmail.com>
2020-12-27 13:11:28 +0200

    Changed the way the model is trained and improved prediction

--- generate.py
+++ generate.py
@@ -1,9 +1,14 @@
 import numpy as np
 from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 import pickle
+import nltk
+
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 
 
-def generate(model, word2index, index2word, method='beam_search', prefix='A'):
+def generate(model, word2index, index2word, seed_text, next_words=20, method='beam_search'):
     if isinstance(model, str):
         model = load_model(model)
     if isinstance(word2index, str):
@@ -11,38 +16,34 @@ def generate(model, word2index, index2word, method='beam_search', prefix='A'):
     if isinstance(index2word, str):
         index2word = pickle.load(open(index2word, 'rb'))
 
-    prefix = word2index[prefix.lower()]
+    max_len = model.input_shape[1]
     if method == 'greedy':
-        sentence = greedy_decode(model, prefix)
+        sentence = greedy_decode(model, word2index, index2word, seed_text, max_len, next_words=next_words)
     elif method == 'beam_search':
-        sequences = beam_search_decode(model, prefix)
-        sentence = sequences[0][0]
+        sentences = beam_search_decode(model, word2index, seed_text, max_len, next_words=next_words)
+        sentence = ' '.join([index2word[token] for token in sentences[0][0]])
 
-    return ' '.join([index2word[token] for token in sentence])
-
-
-def greedy_decode(model, prefix):
-    sentence = [prefix]
-    for token in [0] + sentence:
-        pred = np.argmax(model.predict([token]))
-    sentence.append(pred)
-
-    while pred != 1:
-        pred = np.argmax(model.predict([pred.item()]))
-        sentence.append(pred)
-
-    return sentence[:-1]
+    return sentence
 
 
-def beam_search_decode(model, prefix, k=5):
-    collected = []
+def greedy_decode(model, word2index, index2word, seed_text, max_sequence_length, next_words):
+    for _ in range(next_words):
+        tokens = [word2index[word] for word in word_tokenize((seed_text.lower()))]
+        tokens = pad_sequences([tokens], maxlen=max_sequence_length, padding='pre')
+        pred = np.argmax(model.predict(tokens))
 
-    sequences = [[[prefix], 0.0]]
-    while True:
+        seed_text += ' ' + index2word[pred]
+    return seed_text
+
+
+def beam_search_decode(model, word2index, seed_text, max_sequence_length, next_words, k=8):
+    sequences = [[[word2index[word] for word in word_tokenize(seed_text.lower())], 0.0]]
+    for _ in range(next_words):
         topk_indices = []
         topk_probas = []
         for seq in sequences:
-            pred = model.predict([seq[0][-1]])[0]
+            tokens = pad_sequences([seq[0]], maxlen=max_sequence_length, padding='pre')
+            pred = model.predict(tokens)[0]
             topk_idx = pred.argsort()[-k:][::-1]
             topk_prob = pred[topk_idx]
 
@@ -52,18 +53,13 @@ def beam_search_decode(model, prefix, k=5):
         all_candidates = []
         for (tokens, score), topk_idx, topk_prob in zip(sequences, topk_indices, topk_probas):
             for idx, prob in zip(topk_idx, topk_prob):
-                if idx == 1:
-                    collected.append([tokens, score])
-                    k -= 1
-                    if k == 0:
-                        return sorted(collected, key=lambda tup: tup[1])
-                else:
-                    all_candidates.append([tokens + [int(idx)], score - np.log(prob)])
+                all_candidates.append([tokens + [int(idx)], score - np.log(prob)])
         sequences = sorted(all_candidates, key=lambda tup: tup[1])[:k]
+    return sequences
 
 
 if __name__ == '__main__':
     model = 'model.h5'
     word2index = 'word2index.pickle'
     index2word = 'index2word.pickle'
-    print(generate(model, word2index, index2word, prefix='i'))
+    print(generate(model, word2index, index2word, 'i'))
--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import pickle
 import numpy as np
 import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Embedding, LSTM, Dropout, Dense, Activation
 import nltk
+
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from generate import generate
@@ -13,47 +15,44 @@ from generate import generate
 def load_dataset(file):
     df = pd.read_csv(file)
 
-    sequences = df.body.dropna().drop_duplicates()
-    sequences = list(sequences.apply(lambda x: word_tokenize((str(x).lower()))))
+    sentences = df.body.dropna().drop_duplicates()
+    sentences_words = list(sentences.apply(lambda x: word_tokenize((str(x).lower()))))
 
-    word_counts = {}
-    for seq in sequences:
-        for word in seq:
-            try:
-                word_counts[word] += 1
-            except KeyError:
-                word_counts[word] = 1
+    # sentences_words = [seq for seq in sentences_words if 200 < len(seq) < 350]
 
-    rare_words = [word for word, count in word_counts.items() if count < 3]
-    sequences = [seq for seq in sequences if set(seq).isdisjoint(rare_words) and len(seq) <= 250 - 2]
-
-    word2index = {'SOS': 0,
-                  'EOS': 1}
-    for seq in sequences:
+    word2index = {'PAD': 0}
+    for seq in sentences_words:
         for word in seq:
             if word not in word2index:
                 word2index[word] = len(word2index)
-
     index2word = {i: w for w, i in word2index.items()}
 
-    sequences = [[0] + [word2index[word] for word in seq] + [1] for seq in sequences]
+    input_sequences = []
+    for sent in sentences_words:
+        sent_tokens = [word2index[word] for word in sent]
+        # for i in range(20, len(sent_tokens)):
+        for i in range(1, len(sent_tokens)):
+            input_sequences.append(sent_tokens[:i + 1])
 
-    X = [np.array(seq[:-1]) for seq in sequences]
-    Y = [to_categorical(seq[1:], len(index2word)) for seq in sequences]
+    max_sequence_len = max([len(x) for x in input_sequences])
+    input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
 
-    return X, Y, word2index, index2word
+    X, Y = input_sequences[:, :-1], to_categorical(input_sequences[:, -1], len(word2index))
+
+    return X, Y, word2index, index2word, max_sequence_len
 
 
 if __name__ == '__main__':
-    X, Y, word2index, index2word = load_dataset('data/dataset.csv')
+    # X, Y, word2index, index2word, max_len = load_dataset('data/dataset.csv')
+    X, Y, word2index, index2word, max_len = load_dataset('data/dataset1.csv')
 
     model = Sequential()
-    model.add(Embedding(len(word2index), 100, input_length=1))
-    model.add(LSTM(500, return_sequences=True))
+    model.add(Embedding(len(word2index), 100, input_length=max_len - 1))
+    model.add(LSTM(200, return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(500, return_sequences=True))
+    model.add(LSTM(200, return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(500))
+    model.add(LSTM(200))
     model.add(Dropout(0.2))
     model.add(Dense(len(word2index)))
     model.add(Activation('softmax'))
@@ -61,15 +60,10 @@ if __name__ == '__main__':
     model.compile(loss='categorical_crossentropy')
     model.summary()
 
-    for epoch in range(3):
-        for step, (x, y) in enumerate(zip(X, Y)):
-            model.fit(x, y, batch_size=250, shuffle=False, verbose=0)
-            # model.reset_states()
-            if step % 100 == 0:
-                print('Epoch: {}, step: {}'.format(epoch, step))
+    model.fit(X, Y, epochs=100, batch_size=128)
 
     pickle.dump(word2index, open('word2index.pickle', 'wb'))
     pickle.dump(index2word, open('index2word.pickle', 'wb'))
     model.save('model.h5')
 
-    print(generate(model, word2index, index2word, method='greedy', prefix='The'))
+    print(generate(model, word2index, index2word, 'The'))
