9293b76e5a82516ff9fb43a1518b48babb261105
Enkhai <aerogtx@gmail.com>
2021-01-01 19:59:24 +0200

    Implemented top-k and top-p sampling

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 
 
-def generate(model, word2index, index2word, seed_text, next_words=100, method='beam_search'):
+def generate(model, word2index, index2word, seed_text, next_words=100, method='sample', k=0, p=0.0, temp=1.0):
     if isinstance(model, str):
         model = load_model(model)
     if isinstance(word2index, str):
@@ -16,26 +16,26 @@ def generate(model, word2index, index2word, seed_text, next_words=100, method='b
         index2word = pickle.load(open(index2word, 'rb'))
 
     max_len = model.input_shape[1]
+    seed_indices = [word2index[word] for word in word_tokenize(seed_text.lower())]
     if method == 'greedy':
-        sentence = greedy_decode(model, word2index, index2word, seed_text, max_len, next_words=next_words)
+        sentence = greedy_decode(model, seed_indices, max_len, next_words=next_words)
     elif method == 'beam_search':
-        sentences = beam_search_decode(model, word2index, seed_text, max_len, next_words=next_words)
-        sentence = ' '.join([index2word[token] for token in sentences[0][0]])
+        sentences = beam_search_decode(model, seed_indices, max_len, next_words, k)
+        sentence = sentences[0][0]
+    elif method == 'sample':
+        sentence = top_sampling(model, seed_indices, max_len, next_words, p, k, temp)
 
-    return sentence
+    return ' '.join([index2word[token] for token in sentence])
 
 
-def greedy_decode(model, word2index, index2word, seed_text, max_sequence_length, next_words):
+def greedy_decode(model, seed, max_sequence_length, next_words):
     for _ in range(next_words):
-        tokens = [word2index[word] for word in word_tokenize((seed_text.lower()))]
-        pred = np.argmax(model.predict([tokens[-max_sequence_length:]]))
-
-        seed_text += ' ' + index2word[pred]
-    return seed_text
+        seed.append(np.argmax(model.predict([seed[-max_sequence_length:]])))
+    return seed
 
 
-def beam_search_decode(model, word2index, seed_text, max_sequence_length, next_words, k=8):
-    sequences = [[[word2index[word] for word in word_tokenize(seed_text.lower())], 0.0]]
+def beam_search_decode(model, seed, max_sequence_length, next_words, k=8):
+    sequences = [[seed, 0.0]]
     for _ in range(next_words):
         topk_indices = []
         topk_probas = []
@@ -55,8 +55,28 @@ def beam_search_decode(model, word2index, seed_text, max_sequence_length, next_w
     return sequences
 
 
+def top_sampling(model, seed, max_sequence_length, next_words, p=0.0, k=0, temperature=1.0):
+    for _ in range(next_words):
+        pred = model.predict([seed[-max_sequence_length:]])[0]
+        seed.append(int(sample(pred, temperature, p, k)))
+    return seed
+
+
+def sample(preds, temperature=1.0, p=0.0, k=0):
+    if k > 0:
+        top_k_indices = preds.argsort()[-k:][::-1]
+        preds = preds[top_k_indices]
+    elif p > 0.0:
+        preds = preds[preds > p]
+    preds = np.log(preds) / temperature
+    preds = np.exp(preds)
+    preds = preds / np.sum(preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
 if __name__ == '__main__':
-    model = 'model.h5'
+    model = 'epochs3seq40model.h5'
     word2index = 'word2index.pickle'
     index2word = 'index2word.pickle'
 
@@ -64,4 +84,5 @@ if __name__ == '__main__':
     # seed_text = 'The man in the white suit tipped his hat. "Why do you keep looking at me like that?", he asked.'
     # seed_text = 'Three children were playing in the park. One of them got up and looked at the other two.'
     seed_text = "Once upon a time two guys were playing around with wires. Suddenly, one of them gets shocked."
-    print(generate(model, word2index, index2word, seed_text))
+    # print(generate(model, word2index, index2word, seed_text, method='beam_search', k=8))
+    print(generate(model, word2index, index2word, seed_text, p=0.003))
--- train.py
+++ train.py
@@ -20,18 +20,19 @@ def load_dataset(file, max_sequence_len=20):
     sentences_words = [seq for seq in sentences_words if max_sequence_len < len(seq)]
 
     word2index = {}
+    index2word = {}
     for seq in sentences_words:
         for word in seq:
             if word not in word2index:
-                word2index[word] = len(word2index)
-    index2word = {i: w for w, i in word2index.items()}
+                i = len(word2index)
+                word2index[word] = i
+                index2word[i] = word
 
     input_sequences = []
     for sent in sentences_words:
         sent_tokens = [word2index[word] for word in sent]
         for i in range(max_sequence_len, len(sent_tokens)):
             input_sequences.append(sent_tokens[i - max_sequence_len:i])
-
     input_sequences = np.array(input_sequences)
 
     X, Y = input_sequences[:, :-1], to_categorical(input_sequences[:, -1], len(word2index))
@@ -55,11 +56,12 @@ if __name__ == '__main__':
     model.compile(loss='categorical_crossentropy')
     model.summary()
 
-    model.fit(X, Y, epochs=20, batch_size=800)
+    epochs = 10
+    model.fit(X, Y, epochs=epochs, batch_size=800)
 
     pickle.dump(word2index, open('word2index.pickle', 'wb'))
     pickle.dump(index2word, open('index2word.pickle', 'wb'))
-    model.save('model.h5')
+    model.save('epochs' + str(epochs) + 'seq' + str(max_len) + 'model.h5')
 
     seed_text = 'The man in the white suit tipped his hat. "Why do you keep looking at me like that?", he asked.'
-    print(generate(model, word2index, index2word, seed_text))
+    print(generate(model, word2index, index2word, seed_text, p=0.003))
